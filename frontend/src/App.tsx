21b1f52a0ad5b9f4d18587dd92422f7f159f5c46
Harley Seixas <developer.hs2021@gmail.com>
2021-09-10 21:19:14 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,8 @@
-import BarChat from "components/BarChart";
-import DataTable from "components/DataTable";
-import DonutChat from "components/DonutChart";
-import Footer from "components/footer";
-import NavBar from "components/NavBar";
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <NavBar />
-
-      <div className="container">
-        <h1 className="text-primary py-3">((Dashboard Vendas))</h1>
-
-        <div className="row px-3">
-        <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Percentual atendimento</h5>
-            <BarChat />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Percentual atendimento</h5>
-            <DonutChat />
-          </div>
-        </div>
-        <div className="py-2">
-          <h2 className="text-primary">Todas Vendas</h2>
-        </div>
-
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+    <Routes />
   );
 }
 
